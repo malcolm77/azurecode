4ae1f10a72746a0a8b89e6956c71ec26abb15f95
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2023-08-16 14:35:23 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php 
 
-echo "Hello World"; 
-echo "this is a simple test"
+echo "Hello World\n"; 
+echo "this is a simple test\n"
 
 
-/* to test
+/* to test */
+  
   if ($stream = fopen('https://wordsstorageaccount.blob.core.windows.net/words/words.txt?sp=r&st=2023-08-16T04:11:14Z&se=2024-08-16T12:11:14Z&spr=https&sv=2022-11-02&sr=b&sig=aGpaCu%2BOruwSG%2FsR44USI56glX%2BlK26KdiBiIdJB%2B2s%3D', 'r')) {
     // print the first 50 bytes
     echo stream_get_contents($stream, 50);
 
     fclose($stream);
 }
-*/
+
 
 ?>
