bbaf05267c88e79e346fb3f358258222ec2c73ce
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2023-08-16 14:23:59 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php echo "Hello World"; ?>
+<?php echo "Hello World"; 
+
+$myFile = "https://wordsstorageaccount.blob.core.windows.net/words/words.txt?sp=r&st=2023-08-16T04:11:14Z&se=2024-08-16T12:11:14Z&spr=https&sv=2022-11-02&sr=b&sig=aGpaCu%2BOruwSG%2FsR44USI56glX%2BlK26KdiBiIdJB%2B2s%3D";
+$lines = file($myFile);//file in to an array
+echo "Todays word is: $lines[1]; //line 2
+
+?>
