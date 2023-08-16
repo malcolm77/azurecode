1c2b17063ed4132e66c3f76c9476d04d89b4fdaf
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2023-08-16 14:32:05 +1000

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,13 @@ echo "Hello World";
 echo "this is a simple test"
 
 
+/* to test
+  if ($stream = fopen('http://www.example.net', 'r')) {
+    // print the first 5 bytes
+    echo stream_get_contents($stream, 5);
+
+    fclose($stream);
+}
+*/
+
 ?>
