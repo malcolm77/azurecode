2b137d0482ec53a645bc937bcbe10f44e5a28543
Malcolm Chalmers <malcolmchalmers@hotmail.com>
2023-08-16 14:34:18 +1000

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ echo "this is a simple test"
 
 
 /* to test
-  if ($stream = fopen('http://www.example.net', 'r')) {
-    // print the first 5 bytes
-    echo stream_get_contents($stream, 5);
+  if ($stream = fopen('https://wordsstorageaccount.blob.core.windows.net/words/words.txt?sp=r&st=2023-08-16T04:11:14Z&se=2024-08-16T12:11:14Z&spr=https&sv=2022-11-02&sr=b&sig=aGpaCu%2BOruwSG%2FsR44USI56glX%2BlK26KdiBiIdJB%2B2s%3D', 'r')) {
+    // print the first 50 bytes
+    echo stream_get_contents($stream, 50);
 
     fclose($stream);
 }
